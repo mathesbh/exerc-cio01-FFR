f08621d33b8fedf4520b1d95b57a3c3be49043a7
mathesbh <mathesbh@gmail.com>
2020-10-28 15:13:15 -0300

    It include MainMenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import { MainMenu } from './components/MainMenu';
 
 function App() {
   return (
     <header className="main-header">
       <div className="main-logo">
         <h1>Logo</h1>
+      </div>
+
+      <div className="header-info">
+        <MainMenu />
       </div>
     </header>
   );
---
+++ src/components/MainMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MainMenu.css'
+
+export const MainMenu = () => (
+    <ul className="main-menu">
+        <li><a href="/#topic-1">topic-1</a></li>
+        <li><a href="/#topic-2">topic-2</a></li>
+        <li><a href="/#topic-3">topic-3</a></li>
+        <li><a href="/#topic-4">topic-4</a></li>
+    </ul>
+)
