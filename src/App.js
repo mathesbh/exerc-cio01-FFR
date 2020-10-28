e93c4b20c9fc85eeadf148dc29de674cebbed4e4
mathesbh <mathesbh@gmail.com>
2020-10-28 14:52:22 -0300

    It include brand

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    
+    <header className="main-header">
+      <div className="main-logo">
+        <h1>Logo</h1>
+      </div>
+    </header>
   );
 }
 
